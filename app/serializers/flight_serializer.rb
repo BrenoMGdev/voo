d20fb148ed86d971bd6e9744503c35377235dc18
BrenoMGdev <brenomoreiradev@gmail.com>
2023-04-26 22:27:08 -0300

    Salvando

--- app/serializers/flight_serializer.rb
+++ app/serializers/flight_serializer.rb
@@ -18,6 +18,6 @@ class FlightSerializer < ActiveModel::Serializer
 	end
 
 	def times
-		object.available_time.map{|t| t.description.strftime("%Y-%m-%dT%H:%M.00Z") }
+		object.available_time.map{|t| t.description.strftime("%H:%M") }
 	end
 end
